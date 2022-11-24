3526f83dfee37d405707469a328686377ce376f9
= <rakib.ivan100@gmail.com>
2022-11-24 23:22:55 +0600

    product and category api done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -24,6 +24,97 @@ async function dbConnection() {
 }
 
 dbConnection();
+
+// Collections
+const products = client.db('guitare').collection('products');
+const categories = client.db('guitare').collection('categories');
+
+
+// products
+app.post('/products', async (req, res) => {
+    try {
+        const result = await products.insertOne(req.body);
+
+        if (result.insertedId) {
+            res.send({
+                success: true,
+                message: `Successfully inserted ${result.insertedId}`
+            })
+        }
+    }
+    catch (error) {
+        console.log(error.name.bgRed, error.message.bold);
+        res.send({
+            success: false,
+            error: error.message,
+        });
+    }
+})
+
+app.get('/products', async (req, res) => {
+    try {
+        const cursor = await products.find({}).toArray();
+        res.send(cursor);
+    }
+    catch (error) {
+        console.log(error.name.bgRed, error.message.bold);
+        res.send({
+            success: false,
+            error: error.message,
+        });
+    }
+})
+
+app.get('/products/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const product = await products.findOne(query);
+        res.send(product);
+    }
+    catch (err) {
+        console.log(err.name.bgRed, err.message.bold);
+        res.send({
+            success: false,
+            error: err.message,
+        });
+    }
+})
+
+// categories
+app.post('/categories', async (req, res) => {
+    try {
+        const result = await categories.insertOne(req.body);
+
+        if (result.insertedId) {
+            res.send({
+                success: true,
+                message: `Successfully inserted ${result.insertedId}`
+            })
+        }
+    }
+    catch (err) {
+        console.log(err.name.bgRed, err.message.bold);
+        res.send({
+            success: false,
+            error: err.message,
+        });
+    }
+})
+
+app.get('/categories', async (req, res) => {
+    try {
+        const cursor = await categories.find({}).toArray();
+        res.send(cursor);
+    }
+    catch (err) {
+        console.log(err.name.bgRed, err.message.bold);
+        res.send({
+            success: false,
+            error: err.message,
+        });
+    }
+})
 
 app.get('/', (req, res) => {
     res.send('server started');

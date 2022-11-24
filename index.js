dd120273d7cf06e98c137b25641edc0f29f4998a
= <rakib.ivan100@gmail.com>
2022-11-24 19:46:53 +0600

    database connected

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
+const { MongoClient } = require('mongodb');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('colors');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 
+// mongodb connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2f4txuh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+
+async function dbConnection() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB'.yellow.bold);
+    }
+    catch (err) {
+        console.log(err.name, bgRed, err.message, bold);
+    }
+}
+
+dbConnection();
 
 app.get('/', (req, res) => {
     res.send('server started');
@@ -14,4 +32,3 @@ app.get('/', (req, res) => {
 app.listen(port, (req, res) => {
     console.log(`server listening on ${port}`);
 })
-
